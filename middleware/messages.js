fb8615330e7c504a1732964c47de64e096dc1b9c
Jose Martin Fe <martin.fesito@gmail.com>
2024-05-31 16:05:59 -0300

    Se corrige estructura de codigo, se agrega funcionalidad donde tambien recibe el mje de la consulta el admin

--- middleware/messages.js
+++ middleware/messages.js
@@ -35,7 +35,60 @@ const recoveryPass = async (emailUsuario) => {
   }
 };
 
+const contacto = async (req, res) => {
+  try {
+    const { nombre, apellido, email, mensaje } = req.body;
+
+    const output = `
+      <p>Hola ${nombre} ${apellido},</p>
+      <p>Gracias por contactarte, en breve nos pondremos en contacto contigo.</p>
+      <p><strong>Mensaje:</strong> ${mensaje}</p>
+    `;
+
+    let mailOptions = {
+      from: `"Veterinaria Patas y Garras" <${process.env.GMAIL_MAIL}>`,
+      to: `${email}`, // Aquí solo se envía al correo del cliente
+      subject: "Gracias por contactarnos",
+      text: `Hola ${nombre} ${apellido}, gracias por contactarte, en breve nos pondremos en contacto contigo.\n\nMensaje de consulta:\n${mensaje}`,
+    };
+
+    // Aquí se reenvía el mensaje al correo configurado en process.env.GMAIL_MAIL
+    let mailOptionsAdmin = {
+      from: `"Veterinaria Patas y Garras" <${process.env.GMAIL_MAIL}>`,
+      to: `${process.env.GMAIL_MAIL}`,
+      subject: `Consulta de ${nombre}`,
+      text: `Mensaje de consulta de ${nombre} ${apellido} :\n\n${mensaje} \n\nMail del cliente: ${email}`,
+    };
+
+    // Envío de correos
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Correo enviado al cliente: " + info.response);
+      }
+    });
+
+    // Envío de correo al administrador
+    transporter.sendMail(mailOptionsAdmin, function (error, info) {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Correo enviado al administrador: " + info.response);
+      }
+    });
+
+    let info = await transporter.sendMail(mailOptions);
+    console.log("Mensaje enviado: %s", info.messageId);
+    res.status(200).json({ message: "Correo enviado" });
+  } catch (error) {
+    console.error("Error al enviar el correo: %s", error);
+    res.status(500).json({ error: "Error al enviar el correo" });
+  }
+};
+
 module.exports = {
   welcomeUser,
   recoveryPass,
+  contacto,
 };
