6f98f72caa4da67284bdb96a4f0fb0e7f5b0a897
Lisandro Moreno <lisandromoreno28@gmail.com>
2024-06-03 17:59:40 -0300

    se trabaja sobre la edicion,eliminacion y creacion de productos creando sus respectivas rutas,controladores y middlewares necesarios

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,17 +2,35 @@ const express = require("express");
 const {
   getProductos,
   getOneProducto,
+  getProductosAdmin,
+  updateProd,
+  addImageProduct,
+  deleteProd,
+  createProd,
 } = require("../controllers/productos.controlador");
 const { check } = require("express-validator");
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer");
 const router = express.Router();
 
 router.get("/", getProductos);
+router.get("/admin", getProductosAdmin);
 router.get(
   "/:id",
   [check("id", "Formato ID incorrecto").isMongoId()],
   getOneProducto
 );
-router.delete("/");
-router.post("/");
+
+router.post("/", auth("admin"), createProd);
+/* router.post(
+  "/addImage/:idProd",
+  multer.single("image"),
+  auth("admin"),
+  addImageProduct
+); */
+
+router.put("/:id", multer.single("image"), updateProd);
+
+router.delete("/:id", auth("admin"), deleteProd);
 
 module.exports = router;
